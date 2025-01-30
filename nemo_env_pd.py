d76121312c373e66e6c9fa286d1e990de5135c06
SavvyProp <ludwigtaycheeying@gmail.com>
2025-01-30 07:39:38 -0500

    changed flatfoot

--- nemo_env_pd.py
+++ nemo_env_pd.py
@@ -490,7 +490,7 @@ class NemoEnv(PipelineEnv):
         rp2 = data.site_xpos[self.right_foot_s2]
         rp3 = data.site_xpos[self.right_foot_s3]
 
-        rew = sites2Rew(lp1, lp2, lp3) * contact[0] + sites2Rew(rp1, rp2, rp3) * contact[1] - 2
+        rew = sites2Rew(lp1, lp2, lp3) * contact[0] + sites2Rew(rp1, rp2, rp3) * contact[1]
 
         return rew
 
