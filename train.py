1e08a794ae80e4b640cfbdfc6af86a843375b9ed
henrytsay <tsay.henry@gmail.com>
2025-02-09 14:58:58 -0500

    checkpoint

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 import dill
 from nemo_env_pd import *
 from nemo_randomize import domain_randomize
+import os
 
 envs.register_environment('nemo', NemoEnv)
 env = envs.get_environment('nemo')
@@ -20,12 +21,22 @@ make_networks_factory = functools.partial(
 pre_model_path = 'walk_policy'
 pre_model = model.load_params(pre_model_path)
 
+checkpoint_dir = 'checkpoints'
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+load_checkpoint_dir = 'load_checkpoints'
+if not os.path.exists(load_checkpoint_dir):
+    load_checkpoint_dir = None
+
+
 train_fn = functools.partial(
       ppo.train, num_timesteps=300000000, num_evals=20, episode_length = 2000,
        normalize_observations=False, unroll_length=20, num_minibatches=64,
       num_updates_per_batch=4, discounting=0.98, learning_rate=3.0e-4,
       entropy_cost=1e-3, num_envs=2048, batch_size=1024,
-      network_factory=make_networks_factory, randomization_fn = domain_randomize)
+      network_factory=make_networks_factory, randomization_fn = domain_randomize,
+      save_checkpoint_path=checkpoint_dir, restore_checkpoint_path=load_checkpoint_dir,)
 
 x_data = []
 y_data = {}
