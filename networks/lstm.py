16dd50eeb7ad8c56ebf059e25e3aa70046020572
SavvyProp <ludwigtaycheeying@gmail.com>
2025-02-11 20:05:43 -0500

    smaller lstm

--- networks/lstm.py
+++ networks/lstm.py
@@ -17,7 +17,7 @@ from jaxopt import *
 #Need custom flax nn, make_policy_network, distribution, and make_ppo_network
 #Action size consits of 128 + 128 + a size
 
-HIDDEN_SIZE = 256
+HIDDEN_SIZE = 128
 DEPTH = 1
 
 class StackedLSTM(nn.Module):
