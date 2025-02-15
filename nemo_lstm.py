80ead9b64d5bcc1a70304cf09b8b6b536caa9a42
SavvyProp <ludwigtaycheeying@gmail.com>
2025-02-14 21:33:48 -0500

    vel sharpness

--- nemo_lstm.py
+++ nemo_lstm.py
@@ -362,7 +362,7 @@ class NemoEnv(PipelineEnv):
         vel = self.pelVel(data0, data1)
         vel_target = state.info["velocity"]
         vel_n = jnp.sum(jnp.square(vel[0:2] - vel_target))
-        return jnp.exp( vel_n * -1 / 0.25)
+        return jnp.exp( vel_n * -1 / 0.1)
 
     def angvelZReward(self, state, data):
         angvel = data.xd.ang[self.pelvis_id][2]
