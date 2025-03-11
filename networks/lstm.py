a86bacfcf1fe7245d8497e54f4d075635ab8b1ab
SavvyProp <ludwigtaycheeying@gmail.com>
2025-03-11 19:09:21 -0400

    1x128 lstm skip

--- networks/lstm.py
+++ networks/lstm.py
@@ -20,7 +20,7 @@ Initializer = Callable[..., Any]
 #Need custom flax nn, make_policy_network, distribution, and make_ppo_network
 #Action size consits of 128 + 128 + a size
 
-HIDDEN_SIZE = 64
+HIDDEN_SIZE = 128
 DEPTH = 1
 
 class StackedLSTM(nn.Module):
@@ -30,7 +30,7 @@ class StackedLSTM(nn.Module):
         self.nn_in1 = nn.Dense(512, name = "i1", kernel_init=self.kernel_init)
         #self.nn_in2 = nn.Dense(256, name = "i2", kernel_init=self.kernel_init)
         #self.nn_mi = nn.Dense(256, name = "mid", kernel_init=self.kernel_init)
-        #self.nn_mi2 = nn.Dense(128, name = "mid2", kernel_init=self.kernel_init)
+        self.nn_mi2 = nn.Dense(512, name = "mid2", kernel_init=self.kernel_init)
         self.nn_ed = nn.Dense(self.param_size, name = "end", kernel_init=self.kernel_init)
         self.lstms = [nn.LSTMCell(HIDDEN_SIZE,
                         name = "lstm_{}".format(c)) for c in range(DEPTH)]
@@ -40,7 +40,8 @@ class StackedLSTM(nn.Module):
         bs = x.shape[:-1]
         carry = x[..., :2 * HIDDEN_SIZE * DEPTH]
         obs = x[..., 2 * HIDDEN_SIZE * DEPTH:]
-        y = nn.swish(self.nn_in1(obs))
+        y_init = nn.swish(self.nn_in1(obs))
+        y = y_init
         #y = nn.swish(self.nn_in2(y))
         hidden = carry[..., :HIDDEN_SIZE * DEPTH]
         hidden = jnp.reshape(hidden, bs + (DEPTH, HIDDEN_SIZE,))
@@ -54,8 +55,8 @@ class StackedLSTM(nn.Module):
             state, y = self.lstms[i]((hidden[..., i, :], cell[..., i, :]), y)
             hidden_next = hidden_next.at[..., i, :].set(state[0])
             cell_next = cell_next.at[..., i, :].set(state[1])
-        #y = nn.swish(self.nn_mi2(y))
-        y2 = self.nn_ed(y)
+        y = nn.swish(self.nn_mi2(y))
+        y2 = self.nn_ed(y + y_init)
         hidden_next = jnp.reshape(hidden_next, bs + (-1,))
         cell_next = jnp.reshape(cell_next, bs + (-1,))
         output = jnp.concat([hidden_next, cell_next, y2], axis = -1)
