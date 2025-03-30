29d9614ab76e6b83b7db4c575a85d9bde6ac2257
SamitGadekar <sgadekar@purdue.edu>
2025-03-30 15:50:48 -0400

    Update lstm_envs.py moving GenBotEnv

--- lstm_envs.py
+++ lstm_envs.py
@@ -8,12 +8,3 @@ class Nemo4Env(NemoEnv):
 class G2Env(NemoEnv):
     def __init__(self):
         super().__init__(rname = "g2")
-
-class GenBotEnv(NemoEnv):
-    def __init__(self, input_toml_file_path):
-        with open(input_toml_file_path, "rb") as f:
-            model_info = tomllib.load(f)
-        model_general = model_info["general"]
-
-        super().__init__(rname = model_general["model_name"])
-        self.toml_file_path = input_toml_file_path
