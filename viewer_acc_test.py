c9a8978b8a64e5b254b517d1317be9a196da14ac
SavvyProp <ludwigtaycheeying@gmail.com>
2025-04-06 22:12:14 -0400

    a

--- viewer_acc_test.py
+++ viewer_acc_test.py
@@ -100,7 +100,7 @@ def tanh2Action(action: jnp.ndarray):
 
 
 make_inference_fn = makeIFN()
-policy_path = 'walk_policy_acc'
+policy_path = 'walk_policy_acc2'
 saved_params = model.load_params(policy_path)
 inference_fn = make_inference_fn(saved_params)
 jit_inference_fn = jax.jit(inference_fn)
