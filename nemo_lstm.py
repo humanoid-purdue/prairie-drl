7f459230e2ff2add2216cd4798d377ade19a2724
claire <141592011+claiream@users.noreply.github.com>
2025-04-20 15:53:21 -0400

    Update nemo_lstm.py
    reverted back, just to make it simpler for now

--- nemo_lstm.py
+++ nemo_lstm.py
@@ -201,7 +201,7 @@ class NemoEnv(PipelineEnv):
         rng, key = jax.random.split(rng)
         event_period = jax.random.uniform(key, shape = [2], minval = 0, maxval = 1)
         event_period = event_period * jnp.array([3, 2.]) + jnp.array([4, 0.])
-        push_interval_steps = jnp.round(push_interval / self.dt).astype(jp.int32)
+       # push_interval_steps = jnp.round(push_interval / self.dt).astype(jp.int32)
 
         state_info = {
             "rng": rng,
@@ -216,7 +216,7 @@ class NemoEnv(PipelineEnv):
             "halt_cmd": 0,
             "event_period": event_period,
             "push_step": 0,
-            "push_interval_steps": push_interval_steps,
+            "push_interval_steps": 100,
         }
         metrics = metrics_dict.copy()
 
